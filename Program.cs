9065392411d18e410840cc4257727522993bb583
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-17 13:49:02 -0500

    :recycle: Move to .NET 6

--- Program.cs
+++ Program.cs
@@ -1,63 +1,65 @@
-﻿using System;
-using System.IO;
-
-
-namespace rat
+﻿internal class Program
 {
-    internal static class Program
+    /// <param name="args">The files to read</param>
+    /// <param name="number">Print Numbers?</param>
+    /// <param name="ShowEnds">Put an "$" at the End?</param>
+    private static void Main(string[] args, bool number = false, bool ShowEnds = false)
     {
-        /// <param name="args">The files to read</param>
-        /// <param name="number">Print Numbers?</param>
-        /// <param name="showEnds">Put an "$" at the End?</param>
-        private static void Main(string[] args, bool number = false, bool showEnds = false)
+        if (args.Length == 0) PrintFromSTDIN();
+
+        string text = "";
+
+        foreach (string file in args)
         {
-            if (args.Length == 0)
+            try
             {
-                while (true)
-                {
-                    Console.WriteLine(Console.ReadLine());
-                }
+                text += File.ReadAllText(file);
             }
-
-            string text = "";
-
-            foreach (string file in args)
+            catch (System.IO.FileNotFoundException)
             {
-                try
-                {
-                    text += File.ReadAllText(file);
-                }
-                catch (System.IO.FileNotFoundException)
-                {
-                    Console.WriteLine($"{file} file not found!");
-                    return;
-                }
+                Console.WriteLine($"{file} file not found!");
+                return;
             }
-
-
-            if (number)
-            {
-                var splitText = text.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
-                text = "";
-
-                for (var i = 0; i < splitText.Length - 1; i++)
-                {
-                    text += $"  {i}   {splitText[i]}\n";
-                }
-            }
-
-            if (showEnds)
-            {
-                var splitText = text.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
-                text = "";
-
-                for (var i = 0; i < splitText.Length - 1; i++)
-                {
-                    text += $"{splitText[i]}$ \n";
-                }
-            }
-
-            Console.Write(text);
         }
+
+
+        if (number) text = AddNumbers(text);
+
+        if (ShowEnds) text = AddEnds(text);
+
+        Console.Write(text);
+    }
+
+    public static void PrintFromSTDIN()
+    {
+        while (true) Console.WriteLine(Console.ReadLine());
+    }
+
+    public static string AddEnds(string text)
+    {
+        var splitText = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+        text = "";
+
+        for (var i = 0; i < splitText.Length - 1; i++)
+        {
+            text += $"{splitText[i]}$ \n";
+        }
+
+        return text;
+    }
+
+    public static string AddNumbers(string text)
+    {
+        var splitText = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+        text = "";
+
+        for (var i = 0; i < splitText.Length - 1; i++)
+        {
+            text += $"  {i}   {splitText[i]}\n";
+        }
+
+        return text;
     }
 }
